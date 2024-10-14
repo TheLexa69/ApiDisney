f053ad3b4a0bfe5b33a3b6e694f7cf95bfa4dc99
Luis Guillermo AG <guille1insua@gmail.com>
2024-10-14 13:13:53 +0200

    esqueleto

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ApiDisney
  Este proyecto está hecho en Java, con conexion api.
+ 
+ https://api.disneyapi.dev/character
+
+ 
---
+++ disneyapi/src/main/java/disneyapi/Main.java
@@ -0,0 +1,7 @@
+package disneyapi;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
